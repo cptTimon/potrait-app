e5d82d275f8f88a5399a5b50d64092854d49bc6b
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-05-23 15:43:24 +0200

    competition-attack

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -10,10 +10,15 @@ exports.add = async (req, res) => {
 
     if(title && author && email && file) { // if fields are not empty...
 
+      if (title.length > 25 || author.length > 50) throw new Error('Too many characters!')
+
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
+      const fileExt = fileName.split('.').slice(-1)[0];
+      if(fileExt === 'gif' || fileExtt === 'jpg' || fileExt === 'png') {
+        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+        await newPhoto.save(); // ...save new photo in DB
+        res.json(newPhoto);
+      } else throw new Error('Wrong file type!');
 
     } else {
       throw new Error('Wrong input!');
