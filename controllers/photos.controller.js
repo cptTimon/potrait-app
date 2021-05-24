caaf74393695ba99371f94d48ca46f89a19e27cd
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-05-24 14:52:07 +0200

    Security

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,4 +1,5 @@
 const Photo = require('../models/photo.model');
+const Voter = require('../models/Voter.model');
 
 /****** SUBMIT PHOTO ********/
 
@@ -10,10 +11,19 @@ exports.add = async (req, res) => {
 
     if(title && author && email && file) { // if fields are not empty...
 
-      if (title.length > 25 || author.length > 50) throw new Error('Too many characters!')
-
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
       const fileExt = fileName.split('.').slice(-1)[0];
+
+      const titlePattern = new RegExp(/^([A-z]|\s|[0-9]|\.){1,25}$/);
+      if (!titlePattern.test(title)) throw new Error('Invalid title pattern...');
+
+      const authorPattern = new RegExp(/^([A-z]|\s|[0-9]|\.){1,50}$/);
+      if (!authorPattern.test(author)) throw new Error('Invalid author pattern...');
+
+      const emailPattern = new RegExp(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.([a-z]{1,6}))$/i);
+      if (!emailPattern.test(email)) throw new Error('Invalid email pattern...');
+
+
       if(fileExt === 'gif' || fileExtt === 'jpg' || fileExt === 'png') {
         const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
         await newPhoto.save(); // ...save new photo in DB
@@ -50,12 +60,24 @@ exports.vote = async (req, res) => {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
     if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
     else {
+      const voter = await Voter.findOne({ user: req.clientIp });
+      if (!voter) {
+        const newVoter = new Voter({ user: req.clientIp, votes: [ photoToUpdate._id ] });
+        await newVoter.save();
+      } else {
+        if (voter.votes.includes(photoToUpdate._id)) {
+          throw new Error('Vote duplicate!');
+        } else {
+          voter.votes.push(photoToUpdate._id);
+          await voter.save();
+        }
+      }
       photoToUpdate.votes++;
       photoToUpdate.save();
       res.send({ message: 'OK' });
     }
   } catch(err) {
-    res.status(500).json(err);
+    res.status(500).json(err.message);
   }
 
 };
